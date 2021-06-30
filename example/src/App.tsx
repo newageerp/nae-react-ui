01a504a3ea2343c137657808963666f69584f480
NewAgeErp <info@newageerp.com>
2021-06-30 10:26:58 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'nae-react-ui'
+import 'nae-react-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
